c74e3e0071252cda9c20439254f1b8db2248951d
darin4 <zvetochek23@mail.ru>
2024-08-07 00:00:53 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
+    const zeroToNine = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+    ];
+
+    const tenToNineteen = [
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+
+    const twentyToNinety = [
+        'twenty', 'thirty', 'forty', 'fifty','sixty','seventy', 'eighty', 'ninety'
+    ];
+
+    const 
+
+
+
   
 }
