7296b44861d233b9a17b9e000b1210cef1184383
darin4 <zvetochek23@mail.ru>
2024-08-07 00:23:51 +0300

    docs: finish

--- src/index.js
+++ src/index.js
@@ -8,10 +8,33 @@ module.exports = function toReadable (number) {
     ];
 
     const twentyToNinety = [
-        'twenty', 'thirty', 'forty', 'fifty','sixty','seventy', 'eighty', 'ninety'
+        '', '', 'twenty', 'thirty', 'forty', 'fifty','sixty','seventy', 'eighty', 'ninety'
     ];
 
-    const 
+    const hundreds = [
+        '', 'one hundred', 'two hundred','three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'
+    ];
+
+    if (number < 10) {
+        return zeroToNine[number];
+    }
+
+    else if (number < 20) {
+        number = number - 10;
+        return tenToNineteen[number];
+    }
+
+    else if (number < 100) {
+        const numberTwentyToNinety = Math.floor(number / 10);
+        return twentyToNinety[Math.floor(numberTwentyToNinety)] + (number % 10 === 0 ? '' : ' ' + zeroToNine[number % 10]);
+    }
+
+    else if(number < 1000) {
+        const numberHundreds = Math.floor(number / 100);
+        return hundreds[numberHundreds] + (number % 100 === 0 ? '' : ' ' + toReadable(number % 100));
+    }
+    else return "Number is out of test data (0-999)";
+
 
 
 
